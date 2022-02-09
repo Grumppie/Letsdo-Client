e3cabe27f28ddaed4d0109499be5b8c44c739296
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-02-09 18:22:36 +0530

    added work in progress

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -102,6 +102,9 @@ const Update = () => {
                     </div>
                 </div> */}
             </form>
+            <div className="work-in-progress">
+                work in progress <img src="https://img.icons8.com/material-outlined/50/000000/work.png" />
+            </div>
         </div>
     );
 
